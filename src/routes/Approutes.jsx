fd98ab3a58ac60e099819e4bd6c191ecfb6a4a87
PhoutthavongDev <thepvongsa9@gmail.com>
2025-03-18 14:24:07 +0700

    update

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from '../pages/home'
+import Home from '../pages/Home'
 import Shop from '../pages/Shop'
 import Cart from '../pages/Cart'
 import History from '../pages/user/History'
